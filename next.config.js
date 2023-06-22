4ca51a7c3908d881e192010dcefd1e93f0f01580
Weronika Luteńko <vera98d@gmail.com>
2023-06-22 23:52:18 +0200

    project setup

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ['tmdb.org', 'themoviedb.org', 'image.tmdb.org'],
+    unoptimized: true,
+    imageQuality: 60,
+  },
+  compiler: {
+    styledComponents: true
+  },
+
+}
