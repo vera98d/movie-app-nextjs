f1ef63a6ea9304b6b5c7f7865edc631bfa7f3be5
Weronika Luteńko <vera98d@gmail.com>
2023-11-03 10:20:07 +0100

    updated service

--- src/services/MovieListService.js
+++ src/services/MovieListService.js
@@ -1,16 +1,19 @@
 class MovieListService {
-    getMovieList = async (listType) => {
-        const url = `https://api.themoviedb.org/3/movie/${listType}?language=en-US`;
-        const options = {
+    constructor() {
+        this.options = {
             method: 'GET',
             headers: {
                 accept: 'application/json',
                 Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
             }
-        };
+        }
+    };
+
+    getMovieList = async (listType) => {
+        const url = `https://api.themoviedb.org/3/movie/${listType}?language=en-US`;
 
         try {
-            const response = await fetch(url, options);
+            const response = await fetch(url, this.options);
             const data = await response.json();
             return data.results;
         }
@@ -26,16 +29,9 @@ class MovieListService {
 
     getMovieDetails = async (movieId) => {
         const url = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`;
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
-            }
-        };
 
         try {
-            const response = await fetch(url, options);
+            const response = await fetch(url, this.options);
             const data = await response.json();
             return data;
         }
@@ -47,16 +43,23 @@ class MovieListService {
 
     findMoviesByQuery = async (query) => {
         const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=true&language=en-US&page=1'`;
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
-            }
-        };
 
         try {
-            const response = await fetch(url, options);
+            const response = await fetch(url, this.options);
+            const data = await response.json();
+            return data.results;
+        }
+        catch (error) {
+            console.error('Error occured while fetching movies:', error);
+            return [];
+        }
+    }
+
+    searchForMoviesByText = async (queryText) => {
+        const url = `https://api.themoviedb.org/3/search/movie?query=${queryText}&include_adult=false&language=en-US&page=1`;
+
+        try {
+            const response = await fetch(url, this.options);
             const data = await response.json();
             return data.results;
         }
